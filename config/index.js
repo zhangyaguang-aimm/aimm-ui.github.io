428ab5c6786dc0ca3ae13fd370a70b3f3c2e6b12
zhangyaguang <nmzyg252@163.com>
2020-06-24 17:59:15 +0800

    fix:缺失的favicon

--- config/index.js
+++ config/index.js
@@ -27,8 +27,8 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: process.env.PORT || 8080,
-    autoOpenBrowser: true,
+    port: process.env.PORT || 3333,
+    autoOpenBrowser: false,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
